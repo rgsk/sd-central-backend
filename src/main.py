adeb73e0db8419da4554321e58733ff61a2d0c75
rgsk <guptarahul70322@gmail.com>
2025-12-27 22:01:35 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 from routers import items
 from sqlmodel import SQLModel
 from db import engine
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
     SQLModel.metadata.create_all(engine)
+    yield
+    # Shutdown logic (optional)
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")

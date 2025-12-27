40104ac9d40d2ae8a626c75f1dd618b773487717
rgsk <guptarahul70322@gmail.com>
2025-12-27 21:47:21 +0530

    fix

--- src/models/item.py
+++ src/models/item.py
@@ -2,19 +2,27 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
+class ItemDB(SQLModel):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
 class ItemBase(SQLModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
 
 
-class Item(ItemBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class Item(ItemBase, ItemDB, table=True):
+    pass
 
 
 class ItemCreate(ItemBase):
     pass
 
 
-class ItemRead(ItemBase):
+class ItemId(SQLModel):
     id: int
+
+
+class ItemRead(ItemBase, ItemId):
+    pass
